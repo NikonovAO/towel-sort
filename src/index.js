20919a54f3facf8dffa48cbfcb855577829b4c0f
NikonovAO <nikonow@bk.ru>
2021-03-11 09:49:20 +0400

    feat:реализована функция, осуществляющая сортировку

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    var ts = [];
+    if (!matrix || matrix.length === 0)
+    {
+        return ts;
+    }
+    else
+    {
+        function compare(a, b) 
+        {
+            if (a > b) return 1;
+            if (a == b) return 0;
+            if (a < b) return -1;
+        }
+        for (let i=0; i<matrix.length; i++)
+        {
+            let sort_matrix = matrix[i].sort(compare);
+            if (i % 2 !== 0)
+            {
+                sort_matrix = sort_matrix.reverse();
+            }
+            ts = ts.concat(sort_matrix);
+        }
+        return ts;
+    }
 }
